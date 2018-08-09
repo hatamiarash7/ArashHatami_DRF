d5a5bb55b64dd2ee909fbc9c35da3c0fc201d317
hatamiarash7 <hatamiarash7@gmail.com>
2018-08-09 15:48:05 +0430

    include module

--- ArashHatami_DRF/settings.py
+++ ArashHatami_DRF/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'arash.apps.ArashConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,8 +74,12 @@ WSGI_APPLICATION = 'ArashHatami_DRF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drf',
+        'USER': 'drf',
+        'PASSWORD': '123456789',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

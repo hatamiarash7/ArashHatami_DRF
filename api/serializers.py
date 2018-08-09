540607ca2111648ed000b351b9f5b9913c954ec5
hatamiarash7 <hatamiarash7@gmail.com>
2018-08-10 01:57:16 +0430

    add device serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,60 @@
 from rest_framework.serializers import ModelSerializer
 
-from main.models import Author
+from main.models import Author, Device
 
 
-class AuthorSerializer(ModelSerializer):
+class AuthorCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Author
+        fields = [
+            'name',
+            'email'
+        ]
+
+
+class AuthorListSerializer(ModelSerializer):
+    class Meta:
+        model = Author
+        fields = [
+            'id',
+            'name'
+        ]
+
+
+class AuthorDetailSerializer(ModelSerializer):
     class Meta:
         model = Author
         fields = [
             'id',
             'name',
             'email'
+        ]
+
+
+class DeviceCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Device
+        fields = [
+            'author',
+            'name',
+        ]
+
+
+class DeviceListSerializer(ModelSerializer):
+    class Meta:
+        model = Device
+        fields = [
+            'id',
+            'name',
+            'author'
+        ]
+
+
+class DeviceDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Device
+        fields = [
+            'id',
+            'name',
+            'author'
         ]

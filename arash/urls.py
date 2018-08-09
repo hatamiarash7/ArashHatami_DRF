ef10ab28e0bf657c7fe82957d39b3442f5bc69e1
hatamiarash7 <hatamiarash7@gmail.com>
2018-08-09 15:47:57 +0430

    add simple view

--- arash/urls.py
+++ arash/urls.py
@@ -1,4 +1,10 @@
 from django.conf.urls import url
-from django.urls import include
+from django.urls import include, path
+
+from . import views
 
 url(r'^api-auth/', include('rest_framework.urls'))
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]

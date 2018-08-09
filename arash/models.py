de8373df8d370be8690295c278bd52d8e208a575
hatamiarash7 <hatamiarash7@gmail.com>
2018-08-09 15:47:45 +0430

    add new models

--- arash/models.py
+++ arash/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField
+
+
+class Device(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)

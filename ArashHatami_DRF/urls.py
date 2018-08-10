b08d6eef59f06b4a8ec74eb85c97ba74b0663c85
hatamiarash7 <hatamiarash7@gmail.com>
2018-08-10 16:30:18 +0430

    add namespace

--- ArashHatami_DRF/urls.py
+++ ArashHatami_DRF/urls.py
@@ -1,8 +1,9 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
-    path('api/', include('api.urls')),
+    url(r'^api/', include('api.urls', namespace='api')),
 ]

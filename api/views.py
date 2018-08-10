6bd4a3392679d1ac797264547e4cb96c374bbf20
hatamiarash7 <hatamiarash7@gmail.com>
2018-08-10 16:30:12 +0430

    add some mehhods

--- api/views.py
+++ api/views.py
@@ -1,25 +1,69 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter)
 from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView, CreateAPIView, \
     RetrieveUpdateAPIView
 from rest_framework.permissions import (
-    IsAuthenticated
-)
+    IsAuthenticated,
+    AllowAny)
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
 
 from api.permissions import IsOwnerOrReadOnly
 from api.serializers import (
     AuthorListSerializer,
     AuthorDetailSerializer,
     AuthorCreateSerializer,
+
     DeviceListSerializer,
     DeviceDetailSerializer,
-    DeviceCreateSerializer
+    DeviceCreateSerializer,
+
+    UserCreateSerializer,
+    UserLoginSerializer
 )
 from main.models import Author, Device
 
+User = get_user_model()
+
+
+class UserCreate(CreateAPIView):
+    serializer_class = UserCreateSerializer
+    queryset = User.objects.all()
+
+
+class UserLogin(APIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            new_data = serializer.data
+            return Response(new_data, status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
 
 class Authors(ListAPIView):
-    queryset = Author.objects.all()
     serializer_class = AuthorListSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    # permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    # DRF filter = http://localhost:8000/api/authors/?search=ar&ordering=id
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['name']
+
+    # builtin django filter
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = Author.objects.all()
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(name__icontains=query)
+            ).distinct()
+        return queryset_list
 
 
 class AuthorCreate(CreateAPIView):
